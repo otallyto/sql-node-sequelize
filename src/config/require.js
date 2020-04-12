852a577f2931a32339134fff0bed80a4af449518
otallyto <rodrigues.tallyto@hotmail.com>
2020-04-12 14:53:37 -0400

    Melhorando a importaçao dos controllers

---
+++ src/config/require.js
@@ -0,0 +1,6 @@
+module.exports = {
+  address: require("../controllers/AddressController"),
+  report: require("../controllers/ReportController"),
+  tech: require("../controllers/TechController"),
+  user: require("../controllers/UserController"),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,21 @@
 const express = require("express");
+const { address, report, tech, user } = require("./config/require");
 const routes = express.Router();
-const UserController = require("./controllers/UserController");
-const AddressController = require("./controllers/AddressController");
-const TechController = require("./controllers/TechController");
-const ReportController = require('./controllers/ReportController')
-
 
 routes.get("/", (req, res) => {
-  res.json({welcome: "API NODE SQL"});
+  res.json({ welcome: "API NODE SQL" });
 });
 
+routes.get("/users", user.index);
+routes.post("/users", user.store);
 
-routes.get("/users", UserController.index);
-routes.post("/users", UserController.store);
+routes.post("/users/:user_id/addresses", address.store);
+routes.get("/users/:user_id/addresses", address.index);
 
-routes.post("/users/:user_id/addresses",AddressController.store)
-routes.get("/users/:user_id/addresses",AddressController.index)
+routes.post("/users/:user_id/techs", tech.store);
+routes.get("/users/:user_id/techs", tech.index);
+routes.delete("/users/:user_id/techs", tech.destroy);
 
-routes.post("/users/:user_id/techs",TechController.store)
-routes.get("/users/:user_id/techs",TechController.index)
-routes.delete("/users/:user_id/techs",TechController.destroy)
-
-routes.get('/report',ReportController.show)
-
-
+routes.get("/report", report.show);
 
 module.exports = routes;
